dc92a3f70fc7898025f8da2b16c62fabbd0fff54
Jaya Wijono <jwijono@seek.com.au>
2021-01-13 18:24:56 +1100

    add spawn process demo code

---
+++ lib/otp_demo/spawn_process.ex
@@ -0,0 +1,21 @@
+# 1. Spawning process
+
+# Enum.map(1..200_000, fn(x) -> spawn(fn -> :timer.sleep(3000); IO.puts("Process #{x} is done!") end) end)
+
+# 2. Sending messages
+
+receiver = spawn(fn -> receive do
+  {:hello, msg} -> IO.puts "Received #{msg}"
+end end)
+#
+Process.alive?(receiver)
+#
+sender = spawn(fn -> send(receiver, {:hello, 'aloha'}) end)
+#
+# Process.alive?(sender)
+# Process.alive?(receiver)
+#
+main_pid = self()
+sender = spawn(fn -> send(main_pid, {:hello, 'aloha'}) end)
+#
+# flush()
